c49d4964bc2d7e2ca99b84f9b5756aa1d05d2732
eva <710737179@qq.com>
2019-02-19 11:22:21 +0800

    reducer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <br/>
+        <h1 className="jumbotron-heading text-center"> { this.props.value } </h1>
+        <p className="text-center">
+          <button onClick={ this.props.onIncrement } className="btn btn-primary mr-2">Increase</button>
+          <button onClick={ this.props.onDecrement } className="btn btn-danger my-2">Decrease</button>
+        </p>
       </div>
     );
   }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import counter from './reducer/counter';
+import { increment, decrement } from './actions/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(counter);
+
+// store.subscribe(() => console.log("State updated", store.getState()));
+
+const render = () => {
+    ReactDOM.render(
+        <App 
+            onIncrement = {() => store.dispatch(increment())} 
+            onDecrement = {() => store.dispatch(decrement())}
+            value={store.getState()}
+        />, document.getElementById('root'));
+}
+
+render();
+store.subscribe(render);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
